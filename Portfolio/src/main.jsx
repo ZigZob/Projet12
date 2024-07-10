be6b340a99c06813b3d3775fc4ea3a527a0fe8f2
Zig <amalric.alex@gmail.com>
2024-07-10 21:12:41 +0200

    and another one

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './_index.scss'
 import { Provider } from "react-redux"
 import { store } from './store/index.js'
-import App from '../../Portfolio/src/containers/App/App.jsx'
+import App from "./containers/App/App.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
