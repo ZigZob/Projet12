6933ac8d753f543c110d32545c5ee57329512276
Zig <amalric.alex@gmail.com>
2024-07-10 09:12:52 +0200

    typos & github svg fill

--- Portfolio/src/containers/hero/Hero.jsx
+++ Portfolio/src/containers/hero/Hero.jsx
@@ -15,8 +15,8 @@ export default function Hero() {
             <h2 className="hero__element">{database[key].hero[2]}<strong className={`hero__element--${time === "night" ? "night" : "day"}`}> {database[key].hero[3]}</strong>.</h2>
             <p className="hero__element hero__description">
                 {lang === "en" ?
-                    "As a web developer, I have enhanced my ability to break down complex problems into a multitude of simpler ones. As a gamer, I have grown to love understanding complex systems. As a deutan, I have always nurtured my unique perspective. As a devil's advocate, I have taken a liking into seeing the world through a different prism to get a better understanding of my surroundings."
-                    : "En tant que développeur web, j'ai amélioré ma capacité a déstructurer des problèmes complexes en une multitude de problèmes simples. En tant que gamer, j'ai su apprécier la comprehension de systèmes complexes. En tant que deutéranope, j'ai depuis toujours cultivé ma différence de point de vue. En tant qu'avocat du diable, j'ai pu prendre plaisir à voir le monde sous un prisme différent du miens afin de mieux comprendre mon environnement."
+                    "As a web developer, I have enhanced my ability to break down complex problems into a multitude of simpler ones. As a gamer, I have grown to love understanding complex systems. As a deutan, I have always nurtured my unique perspective. As a devil's advocate, I have taken a liking to seeing the world through a different prism to get a better understanding of my surroundings."
+                    : "En tant que développeur web, j'ai amélioré ma capacité à déstructurer des problèmes complexes en une multitude de problèmes simples. En tant que gamer, j'ai su apprécier la comprehension de systèmes complexes. En tant que deutéranope, j'ai depuis toujours cultivé ma différence de point de vue. En tant qu'avocat du diable, j'ai pu prendre plaisir à voir le monde sous un prisme différent du mien afin de mieux comprendre mon environnement."
                 }
             </p>
         </section>
