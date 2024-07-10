4778e426950f767f89f84e69c45c0a336a089825
Zig <amalric.alex@gmail.com>
2024-07-10 19:42:40 +0200

    updated img paths for production

--- Portfolio/src/components/tag/Index.jsx
+++ Portfolio/src/components/tag/Index.jsx
@@ -4,6 +4,6 @@ import "./_index.scss"
 export default function Tag({ content }) {
 
     return (
-        <img className="tag" src={`/${content}.svg`} alt={content} aria-label={content} title={content} />
+        <img className="tag" src={`/Portfolio/${content}.svg`} alt={content} aria-label={content} title={content} />
     );
 };
