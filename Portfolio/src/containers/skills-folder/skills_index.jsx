d6e4731e3da3218826171378b597da3727a92148
Zig <amalric.alex@gmail.com>
2024-07-24 01:21:01 +0200

    added focus-visible, removed h2 tabIndex, px>rem on hero

--- Portfolio/src/containers/skills-folder/skills_index.jsx
+++ Portfolio/src/containers/skills-folder/skills_index.jsx
@@ -8,8 +8,8 @@ import Skill from "../../components/skill-folder/skill_index"
 export default function Skills() {
     const lang = useSelector(selectLang)
     return (
-        <section tabIndex={0} className="skills" >
-            <h2 tabIndex="0" className="skills__header" >{lang === "en" ? "Skills I've developed :" : "Compétences appliquées :"}</h2>
+        <section tabIndex="0" className="skills" >
+            <h2 className="skills__header" >{lang === "en" ? "Skills I've developed :" : "Compétences appliquées :"}</h2>
             <ul className="skills__list" >
                 {skillsList[lang].map((skill, index) => (
                     <Skill key={index} content={skill[0]} emphasis={skill[1]} add={skill[2]} />
